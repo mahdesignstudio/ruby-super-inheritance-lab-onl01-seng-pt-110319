c89953c976f85503caf77f41ffe253c1729ca024
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-19 01:48:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 class Student 
   def hello 
-   puts "Hey there! I'm so excited to learn stuff.\n"
+      puts "Hey there! I'm so excited to learn stuff.\n"
   end 
   
   def raise_hand 
-  puts "Pick me!\n"
+      puts "Pick me!\n"
   end 
 end 
