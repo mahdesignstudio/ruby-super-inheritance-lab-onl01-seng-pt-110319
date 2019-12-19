899a8b3eba2a0bf3e9c415a9246af36b644e491d
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-19 01:52:28 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,7 @@ require_relative "./student.rb"
 class ChattyStudent < Student 
   def hello 
     super 
-    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end 
   
   def raise_hand 
